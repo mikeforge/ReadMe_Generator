5d6e172abe11c1bc4eb0ac9b5050075a27b461ae
Mike Kelly <mike.forge@gmail.com>
2022-01-24 14:43:43 -0500

    initial load - getting started

---
+++ index.js
@@ -0,0 +1,58 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+// const questions = [];
+
+
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'How would you describe your project?',
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'How is your project intended to be used?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps to install your project?',
+      },
+      {
+        type: 'input',
+        name: 'License',
+        message: 'What are the licenses required for this project?',
+        choices: ['MIT', 'GNU'],
+        default: ["MIT"],
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the tests for your project?',
+      },
+      {
+        type: 'input',
+        name: 'Contributors',
+        message: 'WHat do future contributors need to know?'
+      }
+    ]);
+  };
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
