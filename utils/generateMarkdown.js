987db1e3d61d604518a3623010ccf5b227df2569
Mike Kelly <mike.forge@gmail.com>
2022-02-19 19:12:35 -0500

    working on formatting and matching between functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,37 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license ==='MIT') {
+  if (license === 'MIT') {
     return `![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)`;
   } else if (license === "GPL") {
     return `![License: GPL](https://img.shields.io/badge/License-GPL-blue.svg)`;
   } else if (license === "Apache") {
     return `![License: Apache](https://img.shields.io/badge/License-Apache-blue.svg)`
-  } else {
+  }
+  else {
     return "";
-  } 
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  }
+  if (license === "Apache") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  }
+  if (license === "GNU") {
+    return "https://opensource.org/licenses/GPL-3.0";
+  }
+  else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
